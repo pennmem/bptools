7f3b7f23acace0172d32832626c1d98074b21fe2
Michael V. DePalatis <mike@depalatis.net>
2017-09-08 11:42:48 -0400

    Document jacksheet DataFrame columns

--- bptools/jacksheet.py
+++ bptools/jacksheet.py
@@ -12,6 +12,16 @@ def read_jacksheet(filename):
     -------
     pd.DataFrame
 
+    Notes
+    -----
+    Columns in the returned :class:`pd.DataFrame`:
+
+    * ``label``: contact labels
+    * ``electrode``: electrode the contact resides on (same as ``label``
+      without the number)
+
+    The index is the jackbox number.
+
     """
     df = pd.read_csv(filename, index_col=0, names=['label'], sep='\s+')
     electrodes = df.label.str.extract(r'(^[a-zA-Z]+)', expand=True) \
