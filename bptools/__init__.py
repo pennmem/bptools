3ea12ebde92fe9c589d72d4604bba824c1121f4d
Michael V. DePalatis <mike@depalatis.net>
2017-09-11 11:36:54 -0400

    Add packaging boilerplate

--- bptools/__init__.py
+++ bptools/__init__.py
@@ -1,2 +1,4 @@
 from .jacksheet import read_jacksheet
 from .pairs import create_pairs
+
+__version__ = "1.0.rc0"
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from bptools import __version__
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='btpools',
+    verison=__version__,
+    packages=find_packages(),
+    package_data={
+        '': ['*.txt', '*.csv'],
+    },
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
+    author='Penn Computational Memory Lab',
+    description='EEG bipolar montage helpers',
+    long_description=readme,
+)
