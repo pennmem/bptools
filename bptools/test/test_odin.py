e00016615d51b052aa8c6ed307ab63fe17e00871
Michael V. DePalatis <mike@depalatis.net>
2017-09-12 14:42:22 -0400

    Add test to ensure primary sense channel is unique

--- bptools/test/test_odin.py
+++ bptools/test/test_odin.py
@@ -1,6 +1,8 @@
 import os.path as osp
 from datetime import datetime
 import pytest
+from io import StringIO
+import pandas as pd
 
 from bptools.odin import make_odin_config, make_config_name
 from bptools.test import datafile, tempdir
@@ -37,4 +39,26 @@ def test_make_odin_config(filename):
     # Saving to a directory
     with tempdir() as path:
         make_odin_config(jfile, 'R1308T_NAME', 0.001, path)
-        assert osp.exists(osp.join(path, 'R1308T_NAME.csv'))
+        outfile = osp.join(path, 'R1308T_NAME.csv')
+        assert osp.exists(outfile)
+
+        # Verify that each primary sense channel is only listed once
+        with open(outfile, 'r') as f:
+            # Read until we get to sense channel definition
+            line = ''
+            while not line.startswith('SenseChannels:'):
+                line = f.readline()
+            print('done')
+
+            sense_channels = []
+            while True:
+                line = f.readline()
+                if 'Stimulation' in line:
+                    break
+                else:
+                    sense_channels.append(line)
+
+        csv = StringIO("".join(sense_channels))
+        config = pd.read_csv(csv, names=['label', 'name', 'c1', 'c2', 'x', 'comment'])
+        assert len(config.c1) == len(config.c1.unique())
+        assert all(config.c1 == config.c1.unique())
