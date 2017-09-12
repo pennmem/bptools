42e508e74b3e5717ff80905595f99f5c8e21b339
Michael V. DePalatis <mike@depalatis.net>
2017-09-12 15:47:06 -0400

    Release version 1.0.0

---
+++ CHANGELOG.rst
@@ -0,0 +1,7 @@
+Version 1.0.0
+=============
+
+**2017-09-12**
+
+Initial release supporting creation of true bipolar electrode configuration
+files for the Odin ENS.
--- bptools/__init__.py
+++ bptools/__init__.py
@@ -3,6 +3,6 @@ from collections import namedtuple
 from .jacksheet import read_jacksheet
 from .pairs import create_pairs
 
-__version__ = "1.0.rc0"
+__version__ = "1.0.0"
 
 version_info = namedtuple("VersionInfo", "major, minor, patch")(*__version__.split('.'))
